5fc189fea67c5256a74920937a4c40ee4af78863
sis39 <sanjanachinni6@gmail.com>
2024-08-19 20:16:59 +0530

    commit changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -93,6 +93,7 @@ const Navbar = (props) => {
             className='border-solid border-coffeeMenu border-2 px-5 py-2 rounded-2xl text-coffeeMenu hover:text-textColor hover:border-textColor hover:bg-coffeeMenu'
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
+            onClick={ReservationTable}
           >
             <span className='text-sm tracking-widest'>MAKE A RESERVATION</span>
           </motion.button>
